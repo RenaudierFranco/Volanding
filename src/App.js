143b53b33e9f83cc1db886440dd81509586753ac
ignacio soria <soria.ign@gmail.com>
2023-09-06 11:36:43 -0300

    Nuevos containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar/NavBar'
+import NavBar from './components/NavBar/NavBar';
+import IitemListContainer from './components/ItemListContainer/ItemListContainer';
+import InputDate from './components/InputDate/InputDate';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <InputDate/>
+      <IitemListContainer/>
     </div>
   );
 }
---
+++ src/components/InputDate/InputDate.jsx
@@ -0,0 +1,65 @@
+import { InputGroup, Form } from 'bootstrap-4-react';
+import { Container, Row, Col } from 'bootstrap-4-react';
+import { Card } from 'bootstrap-4-react';
+
+const InputDate = () => {
+
+    return (
+
+            <div>
+S
+                <Card>
+                <Card.Body>
+                <Container>
+                    <Row>
+                        <Col>
+                            <InputGroup mb="2">
+                                <InputGroup.Prepend><InputGroup.Text>Origen</InputGroup.Text></InputGroup.Prepend>
+                                <Form.Input type="text" placeholder="Origen" />
+                            </InputGroup>
+                        </Col>
+                        <Col>
+                        <InputGroup mb="2">
+                            <InputGroup.Prepend><InputGroup.Text>Destino</InputGroup.Text></InputGroup.Prepend>
+                            <Form.Input type="text" placeholder="Destino" />
+                        </InputGroup>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col></Col>
+                        <Col>
+                            <Form.CustomCheckbox id="customCheck1" m="2">
+                                Solo ida
+                            </Form.CustomCheckbox>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+
+
+
+                    <Row>
+                        <Col>
+                            <InputGroup mb="2">
+                                <InputGroup.Prepend><InputGroup.Text>Fecha de partida</InputGroup.Text></InputGroup.Prepend>
+                                <Form.Input type="date" placeholder="Fecha de partida" />
+                            </InputGroup>
+                        </Col>
+                        <Col>
+                        <InputGroup mb="2">
+                            <InputGroup.Prepend><InputGroup.Text>Fecha de llegada</InputGroup.Text></InputGroup.Prepend>
+                            <Form.Input type="date" placeholder="Fecha de llegada" />
+                        </InputGroup>
+                        </Col>
+                    </Row>
+                </Container>
+                </Card.Body>
+                </Card>
+
+
+            </div>
+
+    )
+}
+
+export default InputDate
