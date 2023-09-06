f9063f1b7b4c1a980fc1a358ce12375f3babf7f1
ignacio soria <soria.ign@gmail.com>
2023-09-06 18:59:10 -0300

    servicio firebase, mapeo de items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
 import LoginScreen from './components/LoginScreen/LoginScreen';
 import FormLogin from './components/FormLogin/FormLogin';
 import FormRegister from './components/FormRegister/FormRegister';
@@ -13,10 +12,8 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-
         <Routes>
           <Route exat path='/' element={<LoginScreen/>} />
-          <Route exat path='/home' element={<NavBar/>} />
           <Route exat path='/LoginScreen' element={<LoginScreen/>} />
           <Route exat path='/FormLogin' element={<FormLogin/>} />
           <Route exat path='/FormRegister' element={<FormRegister/>} /> 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import ItemList from "../ItemList/ItemList";
+import NavBar from "../NavBar/NavBar";
+import { db } from "../../Services/Firebase/Firebase";
+import { collection, getDocs } from 'firebase/firestore'
 
 
 
 const ItemListContainer = () => {
 
+const [items, setItems] = useState([])
+
+useEffect(()=> {
+    getDocs(collection(db, 'Destino')).then((snapshot)=>{
+        const Destino = snapshot.docs.map(doc =>{
+            return {id: doc.id, ...doc.data()}
+          })
+          console.log(`Productos Firebase: ${Destino}`)
+          setItems(Destino)
+          console.log(items)
+    })
+
+}, [])
+
     return (
         <div>
-            <h1>Item List Container</h1>
-            <ItemList/>
+            <NavBar/>
+            <ItemList items={items}/>
         </div>
         
     )
