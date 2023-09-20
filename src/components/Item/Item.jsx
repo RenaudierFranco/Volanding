3ce3767a4b0ea9082a39740fb1e0f5f64e43319d
RenaudierFranco <renaudierfranco@gmail.com>
2023-09-20 15:04:03 -0300

    commit

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,6 +3,7 @@ import { Button } from 'bootstrap-4-react/lib/components';
 import { Col } from 'bootstrap-4-react/lib/components/layout';
 import { NavLink } from 'react-router-dom';
 
+/*
 const Item = ({item}) => {
 
     return (
@@ -26,5 +27,43 @@ const Item = ({item}) => {
       </Col>   
     )
 }
+*/
+
+
+
+import FlightCollection from '../../Services/Collections/FlightCollection.json';
+
+const Item = ({ item }) => {
+  return (
+    <Col col="lg-4 md-6 sm-12">
+      <Card text="center" h="100" shadow p="3" bg="light" rounded>
+        <Card.Header>
+          <Card.Title>
+            {item.departure} - {item.arrival}
+          </Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>Fecha: {item.date}</Card.Title>
+          <Card.Title>Horario: {item.time}</Card.Title>
+          <Card.Subtitle mb="2" text="muted">U$S {item.price}.00</Card.Subtitle>
+          <Card.Text>Avion: {item.plane} / Plazas disponibles: {item.seat}</Card.Text>
+          <Card.Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6OQXO2Y5Wg3-wavb5rXZTgI71ukhEft_zdg&usqp=CAU" />
+          <Button><NavLink to={`/item/${item.id}`}>Ver detalle</NavLink></Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+const ItemList = () => {
+
+  return (
+    <div>
+      {FlightCollection.map((flight) => (
+        <Item key={flight.id} item={flight} />
+      ))}
+    </div>
+  );
+};
 
 export default Item;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import ItemList from "../ItemList/ItemList";
-import { db } from "../../Services/Firebase/Firebase";
-import { collection, getDocs } from 'firebase/firestore'
+//import { db } from "../../Services/Firebase/Firebase";
+//import { collection, getDocs } from 'firebase/firestore';
+import "../../Services/Collections/FlightCollection.json";
 
+
+/*
 const ItemListContainer = ({data}) => {
 
 const [items, setItems] = useState([]);
@@ -20,9 +23,10 @@ useEffect(()=> {
     })
 }, [data])
 
-    return (
-        <>
-        {items.length === 0?
+
+  return (
+    <>
+      {items.length === 0?
         <div className="d-flex justify-content-center mb-5">
             <div className="spinner-border" role="status">
                 <span className="visually-hidden">Loading...</span>
@@ -30,9 +34,46 @@ useEffect(()=> {
         </div>
         :
         <ItemList items={items}/>
-        }
-        </>
-    )
+      }
+    </>
+  )
 }
+*/
+
+
+const ItemListContainer = ({ data }) => {
+const [items, setItems] = useState([]);
+
+useEffect(() => {
+  if (data === undefined) {
+    fetch('./FlightCollection.json')
+      .then((response) => response.json())
+      .then((json) => {
+        if (Array.isArray(json)) {
+          setItems(json);
+        } else {
+          console.error("JSON data is not an array.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error loading JSON data:", error);
+      });
+  }
+}, [data]);
+
+  return (
+    <>
+      {items.length === 0 ? (
+        <div className="d-flex justify-content-center mb-5">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <ItemList items={items} />
+      )}
+    </>
+  );
+};
 
 export default ItemListContainer;
