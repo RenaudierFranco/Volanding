dfcb34d879554e0919ea805b5aa305496d6e6757
ignacio soria <soria.ign@gmail.com>
2023-10-02 12:53:16 -0300

    c

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import {Navbar} from 'bootstrap-4-react';
 import { NavLink } from 'react-router-dom';
 import FormLogin from '../FormLogin/FormLogin';
